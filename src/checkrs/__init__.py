9c5cc2290806728019d895661f50b7b52bf29d04
Timothy Brathwaite <timothyb0912@gmail.com>
2020-09-25 19:01:51 -0700

    Adding initial package files

--- src/checkrs/__init__.py
+++ src/checkrs/__init__.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import DistributionNotFound, get_distribution
+"""
+Visualization module for PyLogit.
+"""
+from __future__ import absolute_import
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
+from .cont_scalars import plot_continous_scalars
+from .disc_scalars import plot_discrete_scalars
+from .marginal import plot_smoothed_marginal
+from .market import plot_simulated_market_shares
+from .reliability import plot_smoothed_reliability
+from .sim_cdf import plot_simulated_cdfs
+from .sim_kde import plot_simulated_kdes
+from .utils import (
+    compute_predictive_log_likelihoods,
+    compute_predictive_mse,
+    is_categorical,
+    progress,
+    simulate_choice_vector,
+)
