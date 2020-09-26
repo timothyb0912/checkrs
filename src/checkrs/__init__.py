91eef189f97b42af7879d1ddc328f815799ff232
Timothy Brathwaite <timothyb0912@gmail.com>
2020-09-25 19:16:00 -0700

    Adding back version info

--- src/checkrs/__init__.py
+++ src/checkrs/__init__.py
@@ -4,6 +4,8 @@ Visualization module for PyLogit.
 """
 from __future__ import absolute_import
 
+from pkg_resources import DistributionNotFound, get_distribution
+
 from .cont_scalars import plot_continous_scalars
 from .disc_scalars import plot_discrete_scalars
 from .marginal import plot_smoothed_marginal
@@ -18,3 +20,12 @@ from .utils import (
     progress,
     simulate_choice_vector,
 )
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
