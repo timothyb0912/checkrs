9daca84a1a18a14fc71af5f0df1f312df083d0cb
Timothy Brathwaite <timothyb0912@gmail.com>
2020-09-27 13:35:49 -0700

    Removing unnecessary imports

--- src/checkrs/__init__.py
+++ src/checkrs/__init__.py
@@ -4,8 +4,6 @@ Tools for simulation-based model checking and diagnostics.
 """
 from __future__ import absolute_import
 
-from pkg_resources import DistributionNotFound, get_distribution
-
 from .cont_scalars import plot_continous_scalars
 from .disc_scalars import plot_discrete_scalars
 from .marginal import plot_smoothed_marginal
