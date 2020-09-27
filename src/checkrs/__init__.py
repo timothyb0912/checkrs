6ef841939be83c4e423647c2ef8e0437b1a62290
Timothy Brathwaite <timothyb0912@gmail.com>
2020-09-27 12:12:56 -0700

    bump2version compliant versioning

--- src/checkrs/__init__.py
+++ src/checkrs/__init__.py
@@ -21,11 +21,4 @@ from .utils import (
     simulate_choice_vector,
 )
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
+__version__ = "0.0.0"
