4922fe23071a28fee0715ce9610f2e68c4ec565d
Timothy Brathwaite <timothy.brathwaite@stitchfix.com>
2020-11-10 13:42:24 -0800

    Skeleton for tests of chart protocol

---
+++ tests/unit/test_chart_protocol.py
@@ -0,0 +1,56 @@
+"""
+This file tests the Chart protocol's contract.
+"""
+import unittest
+from itertools import product
+from typing import List
+
+from checkrs.base import ChartData, View
+from checkrs.sim_cdf import ViewSimCDF
+
+
+
+class ProtocolTests(unittest.TestCase):
+    """
+    Testing basic properties of a Checkrs.Chart instance.
+    Are the signatures correct?
+    """
+    temp_dir = "_tmp/"
+    charts_all: List[View] = [ViewSimCDF]
+
+    @property
+    def data(self) -> ChartData:
+        pass
+
+    def test_draw_signature(self):
+        """
+        GIVEN a chart instantiated with valid_chart_data
+        WHEN we call the draw method with any valid keyword-argument
+        THEN we receive the appropriate matplotlib.Figure or an altair.Chart
+        """
+        for backend, chart_class in product(BACKENDS, self.charts_all):
+            chart = chart_class(self.data)
+            manipulable_object = chart.draw(backend=backend)
+            self.assertIsInstance(manipulable_object, ChartObjectType)
+
+    def test_save_functionality(self):
+        """
+        GIVEN a chart instantiated with valid_chart_data
+        WHEN we call the save method with any valid keyword-argument
+        THEN the appropriate file will be saved to its appropriate location
+        """
+        if not os.path.isdir(self.temp_dir):
+            os.mkdir(self.temp_dir)  # Make a directory to hold the test plots
+        filename = temp_dir + "test_filename"
+        extensions = [".png", ".pdf", ".json", ".html"]
+        try:
+            for ext, chart_class in product(extensions, CHARTS_ALL):
+                chart = chart_class(self.data)
+                full_path_current = filename + ext
+                # Ensure missing file, create the file, ensure existing file
+                self.assertFalse(os.path.exists(full_path_current))
+                chart.save(full_path_current)
+                self.assertTrue(os.path.exists(full_path_current))
+        except:
+            os.rmdir(temp_dir)  # Clear up test plots even if failure happens
+            raise  # Re-raise last exception
